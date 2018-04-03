286de00ad70015333a30d62b9363d8644b083e8c
killianmh <killianmh@gmail.com>
2018-04-02 19:11:07 -0700

    finished program; added readme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # liri-node-app
+
+This app runs in Git Bash. To call it, use "node liri.js" and then pick one of the following commands:
+
+-my-tweets: gives the last 20 tweets of my account
+-spotify-this-song: returns song information about the song entered
+-movie-this: returns movie information about the movie entered
+-do-what-it-says: reads from the random.txt file and executes the command in that file (this can be changed to any of the three commands above)
+
--- liri.js
+++ liri.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+var request = require("request");
+var fs = require("fs");
+
 var keys = require("./keys");
 var twitter = require("twitter");
 var Spotify = require("node-spotify-api");
@@ -16,7 +19,8 @@ else if(command === "spotify-this-song"){
 	spotify_this_song(song);
 }
 else if(command === "movie-this"){
-	movie_this();
+	var movie = process.argv[3];
+	movie_this(movie);
 }
 else if(command === "do-what-it-says"){
 	do_what_it_says();
@@ -43,32 +47,130 @@ function my_tweets(){
 
 function spotify_this_song(song){
 	if(song === undefined){
-		song = "The Sign";
+		spotify.request("https://api.spotify.com/v1/search?q=track:The%20Sign%20artist:Ace%20of%20Base&type=track&limit=1")
+		.then(function(data){
+			console.log("\n");
+			console.log("Artist:");
+			console.log(data.tracks.items[0].artists[0].name + "\n");
+			console.log("Song Name:");
+			console.log(data.tracks.items[0].name + "\n");
+			console.log("Preview URL:");
+			console.log(data.tracks.items[0].preview_url + "\n");
+			console.log("Album:");
+			console.log(data.tracks.items[0].album.name + "\n");
+
+		})
 	}
-	spotify.search({type: "track", query: song, limit: 1}, function(error, data){
-		if(error){
-			return console.log(error);
-		}
-		var result = JSON.stringify(data);
-		var artistCount = data.tracks.items[0].artists.length;
-		console.log("");
-		console.log("Artist(s):");
-			for(i = 0; i< artistCount; i++){
-				console.log("-" + data.tracks.items[0].artists[i].name);
+	else{
+		spotify.search({type: "track", query: song, limit: 1}, function(error, data){
+			if(error){
+				return console.log("Not a valid song name; please try again");
 			}
-		console.log("");
-		console.log("Song Name:");
-		console.log(data.tracks.items[0].name);
-		console.log(data.tracks);
-		// console.log(data.tracks.items[0].artists[0].name);
-	});
+			var artistCount = data.tracks.items[0].artists.length;
+			console.log("");
+			console.log("Artist(s):");
+				for(i = 0; i< artistCount; i++){
+					console.log("-" + data.tracks.items[0].artists[i].name);
+				}
+			console.log("");
+			console.log("Song Name:");
+			console.log(data.tracks.items[0].name + "\n");
+			console.log("Preview URL:");
+			console.log(data.tracks.items[0].preview_url + "\n");
+			console.log("Album:");
+			console.log(data.tracks.items[0].album.name + "\n");
+		});	
+	}
 }
 
-function movie_this(){
+function movie_this(movie){
+	if(movie === undefined){
+		var queryURL = "https://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
+		request(queryURL, function(error, response, body){
+			if(error){
+				return console.log("error: " + error);
+			}
+			// body = JSON.stringify(body);
+			else {
+				console.log("");
+				console.log("Movie:");
+				console.log(JSON.parse(body).Title + "\n");
+				console.log("Release Year:");
+				console.log(JSON.parse(body).Year + "\n");
+				console.log("IMDB Rating:");
+				console.log(JSON.parse(body).imdbRating + "\n");
+				console.log("Rotten Tomatoes Rating:");
+				console.log(JSON.parse(body)["Ratings"][1].Value + "\n");
+				console.log("Country of Production:");
+				console.log(JSON.parse(body).Country + "\n");
+				console.log("Language:");
+				console.log(JSON.parse(body).Language + "\n");
+				console.log("Plot:");
+				console.log(JSON.parse(body).Plot + "\n");
+				console.log("Actors:");
+				console.log(JSON.parse(body).Actors + "\n");
+			}
 
+		})
+	}
+	else{
+		var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+		request(queryURL, function(error, response, body){
+			if(error){
+				return console.log("error: " + error);
+			}
+			// body = JSON.stringify(body);
+			else{
+				console.log("");
+				console.log("Movie:");
+				console.log(JSON.parse(body).Title + "\n");
+				console.log("Release Year:");
+				console.log(JSON.parse(body).Year + "\n");
+				console.log("IMDB Rating:");
+				console.log(JSON.parse(body).imdbRating + "\n");
+				console.log("Rotten Tomatoes Rating:");
+				console.log(JSON.parse(body)["Ratings"][1].Value + "\n");
+				console.log("Country of Production:");
+				console.log(JSON.parse(body).Country + "\n");
+				console.log("Language:");
+				console.log(JSON.parse(body).Language + "\n");
+				console.log("Plot:");
+				console.log(JSON.parse(body).Plot + "\n");
+				console.log("Actors:");
+				console.log(JSON.parse(body).Actors + "\n");
+			}
+			
+		})
+	}
 }
 
 function do_what_it_says(){
+	fs.readFile("random.txt", "utf8", function(error, data){
+		if(error){
+			return console.log(error);
+		}
+		var text = data.split(",");
+		var action = text[0];
+		var thing = text[1];
 
+		if(action === "my-tweets"){
+			my_tweets();
+		}
+		else if(action === "spotify-this-song"){
+			var song = thing;
+			spotify_this_song(song);
+		}
+		else if(action === "movie-this"){
+			var movie = thing;
+			movie_this(movie);
+		}
+		else if(action === "do-what-it-says"){
+			console.log("Invalid; this will cause an infinte loop!")
+		}
+		else{
+			console.log("Please type a valid command in random.txt")
+		}
+
+	})
 }
 
